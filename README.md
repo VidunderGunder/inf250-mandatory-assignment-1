7ea249c8f5421c7e73beeaa1fd95f859489a0956
sventhevenn <kristiankramas@outlook.com>
2020-09-23 20:30:42 +0200

    Done

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-In this exercise you will write a python-program that reads an image and makes a threshold using the [Otsu algorithm](https://en.wikipedia.org/wiki/Otsu%27s_method).
+# INF250: Mandatory Exercise 1
 
-You will use [this skeleton program](https://nmbu.instructure.com/courses/5600/files/1019649/download) in Python. It consists of one function making a histogram, one that carries out the Outsu thresholding and one function that uses this to make a binary image.
+Task: _In this exercise you will write a python-program that reads an image and makes a threshold using the [Otsu algorithm](https://en.wikipedia.org/wiki/Otsu%27s_method)._
 
-You will hand in the python code on Canvas.
+_You will use [this skeleton program](https://nmbu.instructure.com/courses/5600/files/1019649/download) in Python. It consists of one function making a histogram, one that carries out the Outsu thresholding and one function that uses this to make a binary image._
+
+_You will hand in the python code on Canvas._
+
+## Features:
+
+- Histogram
+- Otsu thresholding
+- Visualization of Otsu's thresholding method
+
+The main script performs the vizualization with both OpenCV's implementation and this assignments manual implementation.
+
+## Comments:
+
+- File based on code template from Yngve Mardal Moe
+- Removed redundant theshold function, as it is only needed if we don't want to use Otsu thresholding
+- Auto-formatted by black with a custom line length  
+  To enable in VSCode, add this to your settings.json:
+
+  ```JSON
+  "python.formatting.provider": "black",
+  "python.formatting.blackArgs": ["-l", "79"],
+  ```
+
+  ...and say yes to any prompts the next time you save a Python file
--- outsu_skeleton.py
+++ outsu_skeleton.py
@@ -1,89 +1,132 @@
 # -*- coding: utf-8 -*-
-
-"""
-Skeleton for first part of the blob-detection coursework as part of INF250
-at NMBU (Autumn 2017).
-"""
+"""INF250 Mandatory assignment 1 - see README.md"""
 
 __author__ = "Kristian Gunder Kramås"
-__email__ = "kristian.gunder.kramas@outlook.com"
+__email__ = "kristiankramas@outlook.com"
 
 import numpy as np
 import cv2
+from skimage.filters import threshold_otsu as cv_otsu
+import matplotlib.pyplot as plt
 
 
-def threshold(image, th=None):
-    """Returns a binarized version of given image, thresholded at given value.
+def create_histogram(image):
+    """Returns the image histogram with 256 bins."""
 
-    Binarises the image using a global threshold `th`. Uses Otsu's method
-    to find optimal threshold value if the threshold variable is None. The
-    returned image will be in the form of an 8-bit unsigned integer array
-    with 255 as white and 0 as black.
-
-    Parameters:
-    -----------
-    image : np.ndarray
-        Image to binarise. If this image is a colour image then the last
-        dimension will be the colour value (as RGB values).
-    th : numeric
-        Threshold value. Uses Otsu's method if this variable is None.
-
-    Returns:
-    --------
-    binarised : np.ndarray(dtype=np.uint8)
-        Image where all pixel values are either 0 or 255.
-    """
-
-    # Setup
-    shape = np.shape(image)
-    binarised = np.zeros([shape[0], shape[1]], dtype=np.uint8)
-
-    if len(shape) == 3:
-        image = image.mean(axis=2)
-    elif len(shape) > 3:
-        raise ValueError('Must be a 2D image')
-
-    if th is None:
-        th = otsu_thval(image)
-
-    # Start thresholding
-    # WRITE YOUR CODE HERE
-
-    return binarised
-
-
-def histogram(image):
-    """Returns the image histogram with 256 bins.
-    """
-
-    # Setup
-    shape = np.shape(image)
+    image = image.ravel()
     histogram = np.zeros(256)
 
-    if len(shape) == 3:
-        image = image.mean(axis=2)
-    elif len(shape) > 3:
-        raise ValueError('Must be at 2D image')
-
-    # Start to make the histogram
-    # WRITE YOUR CODE HERE
+    for value in image:
+        histogram[value] += 1
 
     return histogram
 
 
 def otsu(image):
-    """Finds the optimal thresholdvalue of given image using Otsu's method.
+    """Finds the optimal threshold value of given image using Otsu's method.
+
+    Based on the following implementation:
+    http://courses.cs.tau.ac.il/pyProg/1112a/recitations/ta10.pdf
     """
-    hist = histogram(image)
-    th = 0
 
-    # WRITE YOUR CODE HERE
+    histogram = create_histogram(image)
 
-    return th
+    # sum the values of all pixels
+    sum_all = 0
+
+    for t in range(256):
+        sum_all += t * histogram[t]
+
+    sum_back, w_back, w_for, v_max, t_best = 0, 0, 0, 0, 0
+    total = image.shape[0] * image.shape[1]
+
+    # Go over all possible thresholds
+    for t in range(256):
+        # Update weights
+        w_back += histogram[t]
+        if w_back == 0:
+            continue
+        w_for = total - w_back
+        if w_for == 0:
+            break
+
+        # Calculate classes means
+        sum_back += t * histogram[t]
+        mean_back = sum_back / w_back
+        mean_forw = (sum_all - sum_back) / w_for
+
+        # Calculate between class variance
+        v_between = w_back * w_for * (mean_back - mean_forw) ** 2
+
+        if v_between > v_max:
+            v_max = v_between
+            t_best = t
+
+    return t_best
+
+
+def visualize(image, threshold, title=None):
+    """Shows a figure of orginal image, histogram and thresholded image.
+
+    Based on:
+    https://scikit-image.org/docs/0.13.x/auto_examples/xx_applications/plot_thresholding.html
+
+    Args:
+        image ([type]): [description]
+        threshold ([type]): [description]
+    """
+    binary = image > threshold
+
+    fig, axes = plt.subplots(ncols=3, figsize=(10, 3))
+
+    if title:
+        fig.suptitle(title, fontsize=14)
+
+    ax = axes.ravel()
+    ax[0] = plt.subplot(1, 3, 1, adjustable="box")
+    ax[1] = plt.subplot(1, 3, 2)
+    ax[2] = plt.subplot(1, 3, 3, sharex=ax[0], sharey=ax[0], adjustable="box")
+
+    ax[0].imshow(image, cmap=plt.cm.gray)
+    ax[0].set_title("Original")
+    ax[0].axis("off")
+
+    # As matplotlib has histograms baked in, we'll use that instead of the
+    # histogram() function. Then we will also support the OpenCV-implementation
+    # with no extra code.
+    ax[1].hist(image.ravel(), bins=256)
+    ax[1].set_title("Histogram")
+    ax[1].axvline(threshold, color="r")
+
+    ax[2].imshow(binary, cmap=plt.cm.gray)
+    ax[2].set_title("Thresholded")
+    ax[2].axis("off")
+
+    plt.subplots_adjust(wspace=0.5, top=0.7)
+    plt.show()
+
+
+def load_gray_image(image):
+    """Returns image in grayscale
+
+    Args:
+        image ([type]): Image file
+
+    Returns:
+        numpy.ndarray: 2D-array of integers 0-255
+    """
+    image = cv2.imread(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
 
 
 if __name__ == "__main__":
-    img = cv2.imread('bees.jpeg')
-    ret, thresh = cv2.threshold(
-        img, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
-    )
+    image = load_gray_image("bie_threshold.jpeg")
+
+    # Scikit version for reference
+    threshold = cv_otsu(image)
+    visualize(image, threshold, "CV2 Implementation")
+
+    # Manual implementation
+    threshold = otsu(image)
+    visualize(image, threshold, "Manual Implementation")
